8b6bde4a77b66a77a834416f91bb73f1d04d5f5b
JoanCoCo <joacoco@inf.upv.es>
2021-09-09 14:25:19 +0200

    User name display added.

--- Assets/Scripts/Network/User.cs
+++ Assets/Scripts/Network/User.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 using MLAPI;
 using MLAPI.NetworkVariable;
 using MLAPI.Messaging;
+using TMPro;
 
 public class User : NetworkBehaviour
 {
     private NetworkVariable<int> m_index = new NetworkVariable<int>();
+    private NetworkVariable<string> m_name = new NetworkVariable<string>();
+
+    [SerializeField] private TMP_Text m_nameField;
 
     public override void NetworkStart()
     {
         if(IsLocalPlayer)
         {
             Messenger<GameObject>.Broadcast(Event.LOCAL_USER_CREATED, gameObject);
+            SetNameServerRpc(PlayerPrefs.GetString(UserPrefKey.USER_NAME));
         }
+
+        m_nameField.text = m_name.Value;
+        m_name.OnValueChanged += OnNameChanged;
+    }
+
+    [ServerRpc]
+    private void SetNameServerRpc(string name)
+    {
+        m_name.Value = name;
+    }
+
+    private void OnNameChanged(string oldName, string newName)
+    {
+        m_nameField.text = newName;
     }
 
     public void AssignIndex(int index)
@@ -46,5 +65,10 @@ public class User : NetworkBehaviour
     private void PlaceAtClientRpc(Vector2 position)
     {
         transform.position = new Vector3(position.x, position.y, transform.position.z);
+    }
+
+    private void OnDestroy()
+    {
+        m_name.OnValueChanged -= OnNameChanged;
     }
 }
